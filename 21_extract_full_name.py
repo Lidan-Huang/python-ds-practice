61ee3fd813b7495f5d5925de255af6d723c41786
Lidan-Huang <huangld0314@gmail.com>
2022-07-08 22:54:56 -0700

    finished 21_extract_full_name function and passed all the tests' git push

--- 21_extract_full_name.py
+++ 21_extract_full_name.py
@@ -14,3 +14,13 @@ def extract_full_names(people):
         >>> extract_full_names(names)
         ['Ada Lovelace', 'Grace Hopper']
     """
+    out = []
+    for person in people:
+        name = []
+        for val in person.values():
+            name.append(val)
+        out.append(" ".join(name))
+    return out
+
+    #use comprehension
+    # return [f"{person["first"]} {person["last"]}" for person in people]
