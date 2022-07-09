d5c42f3146e784cc0785ae930f5c96166aebc5ea
Lidan-Huang <huangld0314@gmail.com>
2022-07-08 21:51:41 -0700

    finished 14_compact function and passed all the tests

--- 14_compact.py
+++ 14_compact.py
@@ -1,6 +1,14 @@
+from xmlrpc.client import FastParser
+
+
 def compact(lst):
     """Return a copy of lst with non-true elements removed.
 
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+    out = []
+    for elem in lst:
+        if elem:
+            out.append(elem)
+    return out
