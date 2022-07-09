3a5a922fa091b59013dd18cd6e842e12dcf2f121
Lidan-Huang <huangld0314@gmail.com>
2022-07-09 16:16:34 -0700

    finished 26_vowel_count function and passed the tests

--- 26_vowel_count.py
+++ 26_vowel_count.py
@@ -7,3 +7,11 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    phrase = phrase.lower()
+    vowels = ["a", "e", "i", "o", "u"]
+    letter_freq = {}
+    for letter in phrase:
+        if letter in vowels:
+            letter_freq[letter] = letter_freq.get(letter, 0) + 1
+        
+    return letter_freq
