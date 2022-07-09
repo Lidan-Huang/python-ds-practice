3411a598486feb41c16730fa6d72ea5810b37ba1
Lidan-Huang <huangld0314@gmail.com>
2022-07-08 22:29:36 -0700

    finished 18_calculate function and passed all the tests

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -14,7 +14,7 @@ def flip_case(phrase, to_swap):
     result = ''
     for ltr in phrase:
         if ltr.lower() == to_swap.lower():
-            if to_swap.lower() == to_swap:
+            if to_swap.lower == to_swap:
                 result += (ltr.lower() if ltr != to_swap else ltr.upper())
             else:
                 result += (ltr.upper() if ltr != to_swap else ltr.lower())
