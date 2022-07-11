1b3ba69e6edab3e8ff9542c8fe31512553cf7289
Lidan-Huang <huangld0314@gmail.com>
2022-07-11 14:29:35 -0700

    finished fs_04_reverse_vowels and passed all the tests

--- fs_04_reverse_vowels.py
+++ fs_04_reverse_vowels.py
@@ -1,7 +1,7 @@
 def reverse_vowels(s):
     """Reverse vowels in a string.
 
-    Characters which re not vowels do not change position in string, but all
+    Characters which are not vowels do not change position in string, but all
     vowels (y is not a vowel), should reverse their order.
 
     >>> reverse_vowels("Hello!")
@@ -19,3 +19,26 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    letters = list(s)
+    vowels = "aeiouAEIOU"
+    left = 0
+    right = len(letters) - 1
+
+    while (left < right):
+        while (left < right):
+            if (letters[left] not in vowels):
+                left += 1
+                continue
+            else:
+                break
+        while (left < right):
+            if (letters[right] not in vowels):
+                right -= 1
+                continue
+            else: 
+                break
+        letters[left], letters[right] = letters[right], letters[left]
+        left += 1
+        right -= 1
+    return "".join(letters)
+            
