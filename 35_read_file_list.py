16c89ae67c40c6ad9b983d9682dcdfee64fe276b
Lidan-Huang <huangld0314@gmail.com>
2022-07-11 14:16:15 -0700

    finished 35_read_file_list function and passed all the tests

--- 35_read_file_list.py
+++ 35_read_file_list.py
@@ -1,3 +1,7 @@
+# L = '''Fido
+#        Whiskey
+#        Dr. Sniffle'''
+
 def read_file_list(filename):
     """Read file and print out each line separately with a "-" before it.
 
@@ -19,3 +23,19 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+    # file1 = open(filename, 'w+')
+    # file1.writelines(L)
+    # file1.close()
+
+    # file2 = open(filename, 'r')
+    # lines = file2.readlines()
+
+    # for line in lines:
+    #     if line:
+    #         print(f"- {line.strip()}")
+    # file2.close()
+
+    file1 = open(filename)
+
+    for line in file1:
+        print(f"- {line.strip()}")
