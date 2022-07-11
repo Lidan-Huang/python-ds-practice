98b427ad38b34e9a23ce47c89ed723c951d8648a
Lidan-Huang <huangld0314@gmail.com>
2022-07-11 16:18:19 -0700

    finished 39_find_greater_numbers function with quadratic runtime

--- 39_find_greater_numbers.py
+++ 39_find_greater_numbers.py
@@ -19,3 +19,13 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    count = 0
+
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[j] > nums[i]:
+                count += 1
+    
+    return count
+
+    # runtime: O(N^2)
